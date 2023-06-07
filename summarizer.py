983522d3f497cbaa23f0ed846058e1d3cfb2e376
Terrence Braganca <113404658+Terrence31@users.noreply.github.com>
2023-06-07 16:27:09 +0530

    Create summarizer.py

---
+++ summarizer.py
@@ -0,0 +1,16 @@
+import torch
+import wikipedia
+from transformers import AutoTokenizer, AutoModelWithLMHead
+
+tokenizer = AutoTokenizer.from_pretrained('t5-base')
+model = AutoModelWithLMHead.from_pretrained('t5-base', return_dict=True)
+
+sequence = wikipedia.summary("George Washington", sentences=8)
+#print(sequence)
+inputs = tokenizer.encode("summarize: " + sequence, return_tensors='pt', max_length=512, truncation=True)
+
+summary_ids = model.generate(inputs, max_length=300, min_length=80, length_penalty=5., num_beams=2)
+
+summary = tokenizer.decode(summary_ids[0])
+
+print(summary)
